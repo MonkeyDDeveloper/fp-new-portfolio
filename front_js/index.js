f8dc45b5692aa640d78d8fe07b193b306e928557
MonkeyDDeveloper <fraydeveloper@gmail.com>
2022-08-27 01:12:13 -0500

    improving the projects area

--- front_js/index.js
+++ front_js/index.js
@@ -19,6 +19,12 @@
                         update_projects_focus()
     }
 
+    window.onmousemove = e => {
+        let side = e.clientX < window.innerWidth / 2 ? 0 : 1000000;
+        // console.log(direction)
+        window.scroll(side, window.scrollY)
+    }
+
     // hrefs.onclick = e => e.stopPropagation()
 
-})(document)
+})(document, window)
