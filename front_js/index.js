8df09b73c77f2bc9d8afd76b93bd7327a5c28e4c
JavierDev <jfray.wxq@gmail.com>
2023-06-24 16:56:23 -0500

    Project updated

--- front_js/index.js
+++ front_js/index.js
@@ -1,30 +1,29 @@
 (()=> {
-    const projects_container = document.querySelector('.projects');
-    const show_projects_message = document.querySelector('.show_projects_message');
 
+    const allGridItems = document.querySelectorAll('.gridItem');
 
-    const update_projects_unfocus = () => {
-        projects_container.classList.remove('focus');
-        show_projects_message.textContent = "Click to show projects (8)"
+    for(let gridItem of allGridItems) {
+        
+        gridItem.addEventListener('mouseover', gridItemMouseOver);
+        gridItem.addEventListener('mouseout', gridItemMouseOut);
+
+    }
+    
+    function gridItemMouseOver(e) {
+        // hacer que todos los elementos grid menos el que ejecuto el evento se vuelvan opacos
+        for(let gridItem of allGridItems) {
+            gridItem.style.opacity = '0.2';
+        }
+
+        this.style.opacity = '1';
     }
 
-    const update_projects_focus = () => {
-        projects_container.classList.add('focus');
-        show_projects_message.textContent = "Click to hide projects"
-    }
+    function gridItemMouseOut(e) {
 
-    projects_container.onclick = () => {
-        projects_container.classList.contains('focus') ? 
-                        update_projects_unfocus(): 
-                        update_projects_focus()
-    }
+        for(let gridItem of allGridItems) {
+            gridItem.style.opacity = '1';
+        }
 
-    window.onmousemove = e => {
-        let side = e.clientX < window.innerWidth / 2 ? 0 : 1000000;
-        // console.log(direction)
-        window.scroll(side, window.scrollY)
     }
-
-    // hrefs.onclick = e => e.stopPropagation()
 
 })(document, window)
